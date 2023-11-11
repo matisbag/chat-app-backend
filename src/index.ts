ea1b7f445d232024bf367b150c7dbe6fa741c502
Matis <matis.baguelin@gmail.com>
2023-11-11 01:09:27 +0100

    prisma added

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,10 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response): void => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
+app.listen(port, (): void => {
   console.log(`Server running at http://localhost:${port}`)
 })
