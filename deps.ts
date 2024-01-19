e3dd0ae7f53fb9d2368beacfdfaf7a956bc91429
Matis <matis.baguelin@gmail.com>
2024-01-19 16:49:17 +0100

    build(oak): update to oak v12.6.2

--- deps.ts
+++ deps.ts
@@ -4,6 +4,6 @@
  * This module re-exports the required methods.
  */
 
-export { Application } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+export { Application } from "https://deno.land/x/oak@v12.6.2/mod.ts";
 
 export { load } from "std/dotenv/mod.ts";
