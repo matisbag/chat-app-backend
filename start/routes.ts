da7fb487087227da5a0c894638a65651c797f194
Matis <matis.baguelin@gmail.com>
2024-06-11 15:48:35 +0200

    feat: auth & register

--- start/routes.ts
+++ start/routes.ts
@@ -8,9 +8,20 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import { middleware } from '#start/kernel'
+const AuthController = () => import('#controllers/auth_controller')
 
 router.get('/', async () => {
   return {
     hello: 'world',
   }
 })
+
+router
+  .group(() => {
+    router.post('login', [AuthController, 'login'])
+    router.get('me', [AuthController, 'me']).use(middleware.auth())
+    router.post('register', [AuthController, 'register'])
+    router.post('logout', [AuthController, 'logout']).use(middleware.auth())
+  })
+  .prefix('auth')
